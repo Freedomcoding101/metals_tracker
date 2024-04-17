a66f239a46a0bac6127d2b85ba0b2c420de2fdae
Freedomcoding101 <97307133+Freedomcoding101@users.noreply.github.com>
2024-04-17 17:42:52 +0930

    CRUD complete

--- tracker/urls.py
+++ tracker/urls.py
@@ -8,5 +8,7 @@ path('', views.homepage, name="homepage"),
 # path('silver/', views.silverpage, name="silverpage"),
 path('update/', views.updatePage, name = "updatePage"),
 path('<str:metal_type>/', views.metalPage, name='metal_page'),
-path('edit/<str:metal_type>/<uuid:pk>/', views.editPage, name='editPage')
+path('edit/<str:metal_type>/<uuid:pk>/', views.editPage, name='editPage'),
+path('delete/<str:metal_type>/<uuid:pk>/', views.deletePage, name='deletePage'),
+
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -69,16 +69,41 @@ def editPage(request, metal_type, pk):
     form = form_class(instance=item)
 
     if request.method == 'POST':
-        print('HAPPY')
         # If the form is submitted, validate and save it
         form = form_class(request.POST, instance=item)
         if form.is_valid():
             form.save()
             return redirect('metal_page', metal_type=metal_type)
     else:
-        print('Sad')
         # For GET requests, render the form with the item data
         form = form_class(instance=item)
 
     context = {'form': form}
     return render(request, 'tracker/metals_form.html', context)
+
+def deletePage(request, metal_type, pk):
+    metal_model = None
+    form_class = None
+
+    # Determine the model class and form class based on the metal_type
+    if metal_type == 'gold':
+        metal_model = Gold
+        form_class = GoldForm
+    elif metal_type == 'silver':
+        metal_model = Silver
+        form_class = SilverForm
+    elif metal_type == 'platinum':
+        metal_model = Platinum
+        form_class = PlatinumForm
+    else:
+        # Handle the error gracefully
+        return HttpResponseServerError("Invalid metal type provided.")
+
+    item = metal_model.objects.get(pk=pk)
+
+    if request.method == 'POST':
+        item.delete()
+        return redirect('metal_page', metal_type=metal_type)
+
+    context = {'item': item}
+    return render(request, 'tracker/delete_template.html', context)
