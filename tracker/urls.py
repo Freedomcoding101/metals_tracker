7a463671a07c5a1b14e1af3d89d83cb045309690
Freedomcoding101 <97307133+Freedomcoding101@users.noreply.github.com>
2024-04-17 18:07:37 +0930

    saving before updating database model

--- tracker/urls.py
+++ tracker/urls.py
@@ -3,9 +3,6 @@ from . import views
 
 urlpatterns = [
 path('', views.homepage, name="homepage"),
-# path('gold/', views.goldpage, name="goldpage"),
-# path('platinum/', views.platinumpage, name="platinumpage"),
-# path('silver/', views.silverpage, name="silverpage"),
 path('update/', views.updatePage, name = "updatePage"),
 path('<str:metal_type>/', views.metalPage, name='metal_page'),
 path('edit/<str:metal_type>/<uuid:pk>/', views.editPage, name='editPage'),
