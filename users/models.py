4254c47d78f84a32bbceec5131589619b794ce2e
Freedomcoding101 <97307133+Freedomcoding101@users.noreply.github.com>
2024-04-19 18:16:25 +0930

    added a new users app and set profiles and reviews in the model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    email =models.EmailField(max_length=500, blank=True, null=True)
+    username = models.CharField(max_length=200, blank=True, null=True)
+    short_intro = models.CharField(max_length=500, blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    profile_image = models.ImageField(null=True, blank=True, upload_to='profiles/',
+                                        default='profiles/user-default.png')
+    social_facebook = models.CharField(max_length=200, blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True,
+                            editable=False)
+
+    def __str__(self):
+        return str(self.user.username)
+
+class Review(models.Model):
+    VOTE_TYPE = (('up', 'Up Vote'),
+                ('down', 'Down Vote')
+    )
+    owener=models.ForeignKey(Profile, null=True, blank=True, on_delete=models.SET_NULL)
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=200, choices = VOTE_TYPE)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                            primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.value
