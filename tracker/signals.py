0d124af1f5791d56dda509d728465710be83cee4
Freedomcoding101 <97307133+Freedomcoding101@users.noreply.github.com>
2024-07-22 05:26:20 +0800

    sell form now working correctly and so is the deletion of a sale

--- tracker/signals.py
+++ tracker/signals.py
@@ -1,6 +1,7 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from .models import Gold, Silver, Platinum, Sale
+from decimal import Decimal
 
 def update_quantity(sender, instance, **kwargs):
     sale = instance
@@ -14,6 +15,14 @@ def update_quantity(sender, instance, **kwargs):
         metal_object = metal_model.objects.get(pk=pk)
         metal_object.quantity += update_quantity
         metal_object.save()
+        if metal_object.initial_weight_unit == 'GRAMS':
+            metal_object.weight_grams = Decimal(metal_object.quantity) * Decimal(metal_object.weight_per_unit)
+            metal_object.weight_troy_oz = Decimal(metal_object.weight_grams) / Decimal(31.1035)
+        elif metal_object.initial_weight_unit == 'TROY_OUNCES':
+            metal_object.weight_troy_oz = Decimal(metal_object.quantity) * Decimal(metal_object.weight_per_unit)
+            metal_object.weight_grams = Decimal(metal_object.weight_troy_oz) * Decimal(31.1035)
+        metal_object.cost_to_purchase = Decimal(metal_object.quantity) * Decimal(metal_object.cost_per_unit)
+        metal_object.save()
         print(f'quantity has been updated for gold')
         
     elif str(content_type) == 'Tracker | silver':
@@ -21,12 +30,28 @@ def update_quantity(sender, instance, **kwargs):
         metal_object = metal_model.objects.get(pk=pk)
         metal_object.quantity += update_quantity
         metal_object.save()
+        if metal_object.initial_weight_unit == 'GRAMS':
+            metal_object.weight_grams = Decimal(metal_object.quantity) * Decimal(metal_object.weight_per_unit)
+            metal_object.weight_troy_oz = Decimal(metal_object.weight_grams) / Decimal(31.1035)
+        elif metal_object.initial_weight_unit == 'TROY_OUNCES':
+            metal_object.weight_troy_oz = Decimal(metal_object.quantity) * Decimal(metal_object.weight_per_unit)
+            metal_object.weight_grams = Decimal(metal_object.weight_troy_oz) * Decimal(31.1035)
+        metal_object.cost_to_purchase = Decimal(metal_object.quantity) * Decimal(metal_object.cost_per_unit)
+        metal_object.save()
         print('quantity has been updated for silver')
         
     elif str(content_type) == 'Tracker | platinum':
         metal_model = Platinum
         metal_object = metal_model.objects.get(pk=pk)
         metal_object.quantity += update_quantity
+        metal_object.save()
+        if metal_object.initial_weight_unit == 'GRAMS':
+            metal_object.weight_grams = Decimal(metal_object.quantity) * Decimal(metal_object.weight_per_unit)
+            metal_object.weight_troy_oz = Decimal(metal_object.weight_grams) / Decimal(31.1035)
+        elif metal_object.initial_weight_unit == 'TROY_OUNCES':
+            metal_object.weight_troy_oz = Decimal(metal_object.quantity) * Decimal(metal_object.weight_per_unit)
+            metal_object.weight_grams = Decimal(metal_object.weight_troy_oz) * Decimal(31.1035)
+        metal_object.cost_to_purchase = Decimal(metal_object.quantity) * Decimal(metal_object.cost_per_unit)
         metal_object.save()
         print('quantity has been updated for platinum')
         
